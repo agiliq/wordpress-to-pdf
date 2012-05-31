880e309b197c6ce918d6aa99b86cceacf0348c3f
bhaskar8088 <bhaskar8088@gmail.com>
2012-06-01 01:04:25 +0530

    removed unnecessary code

--- post-to-pdf-convertor/post_to_pdf.php
+++ post-to-pdf-convertor/post_to_pdf.php
@@ -20,13 +20,13 @@ require_once("dompdf/dompdf_config.inc.php");
 	$posts = $wpdb->get_results("select post_title,post_content from $wpdb->posts where post_status='publish'",ARRAY_A);
 	
      
-	$tabl = "<form name='f1' action='".plugins_url()."/post_to_pdf/getpdf.php' method='post'><table id='showposts_table'>";
+	$tabl = "<table id='showposts_table'>";
 	$i=0;
 	foreach($posts as $post){
-		$tabl.= "<tr><td>".($i+1)."</td><td>".$post['post_title']."</td><td></td><td><a href=".plugins_url()."/post_to_pdf/getpdf.php?p=\"".urlencode($post['post_content'])."\"  >Download</a></td></tr>";
+		$tabl.= "<tr><td>".($i+1)."</td><td>".$post['post_title']."</td><td><a href=".plugins_url()."/post_to_pdf/getpdf.php?p=\"".urlencode($post['post_content'])."\"  >Download</a></td></tr>";
 	    $i++;
 	}
-		$tabl.= "</table></form><br>";
+		$tabl.= "</table><br>";
 echo $tabl;
 }
 
